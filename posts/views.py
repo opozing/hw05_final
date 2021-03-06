8ef7dc93d883d21956bd9ee140e8610005bd1ec1
Сергей <gnom213@yandex.ru>
2021-03-06 19:45:01 +0400

    third push

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+
 from yatube.settings import PAG_CONST
+
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post
 
